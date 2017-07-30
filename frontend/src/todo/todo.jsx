f53c8d9e768c0be275863df4e7e053032df5088e
Diego Correa <diegocorrea.cc@gmail.com>
2017-07-30 20:32:08 -0300

    integracao adicionando informacao no node rest

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3000/api/todos'
+
 export default class Todo extends Component {
     constructor(props){
         super(props)
+        this.state = { description: '',  list: []}
+
+        this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
     }
+    handleChange(e){
+      this.setState({...this.state, description: e.target.value})
+    }
     handleAdd(){
-      console.log(this)
+      const description = this.state.description
+
+      axios.post(URL, {description})
+      .then(resp => console.log('Salvou'))
+
     }
     render() {
       return (
         <div>
             <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-            <TodoForm handleAdd={this.handleAdd}/>
+            <TodoForm handleAdd={this.handleAdd}
+              handleChange={this.handleChange}
+            description={this.state.description}/>
             <TodoList/>
         </div>
       )
